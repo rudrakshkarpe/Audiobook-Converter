c895bb1f8b3ee4a65186a038151dd815dd0656dd
rudraskhkarpe <rudraksh.karpe.cs@ghrcem.raisoni.net>
2021-08-12 02:02:43 +0530

    First Project

---
+++ Audiobook.py
@@ -0,0 +1,14 @@
+import PyPDF2
+import pyttsx3
+pdfReader = PyPDF2.PdfFileReader(open('Audiobook.pdf', 'rb'))
+speaker = pyttsx3.init()
+
+for pageNum in range(pdfReader.numPages):
+    pageObj = pdfReader.getPage(pageNum)
+    text = pageObj.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+speaker.stop()
+
+speaker.save_to_file(text, 'audio.mp3')
+speaker.runAndWait()    
